3ec3326626354abfdc5c856ed346cf7e6c37cb9f
PaulD4vd <pauldaviddjukardi@gmail.com>
2025-09-30 13:37:02 +0700

    belajar costructor

---
+++ belajar_constructor_init.py
@@ -0,0 +1,13 @@
+class Hero: # template
+
+    def __init__(self, umur):
+        print("hello", umur)
+
+
+hero1 = Hero(10)
+
+hero2 = Hero(11)
+
+class orang:
+    def __init__(self, nama_orang):
+        self.nama 
