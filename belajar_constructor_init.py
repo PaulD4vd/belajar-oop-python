61eb3d8e52e7a057afd21f31d0b47e5a7de10b49
PaulD4vd <pauldaviddjukardi@gmail.com>
2025-09-30 15:55:06 +0700

    update

--- belajar_constructor_init.py
+++ belajar_constructor_init.py
@@ -9,5 +9,21 @@ hero1 = Hero(10)
 hero2 = Hero(11)
 
 class orang:
-    def __init__(self, nama_orang):
-        self.nama 
+    def __init__(self, nama_orang, umur):
+        self.nama = nama_orang
+        
+        if umur < 0: 
+            raise ValueError("umur gaboleh negatif")
+        else: 
+            self.usia = umur
+
+
+
+orang1 = orang("David", 20)
+orang2 = orang("andhika", 14)
+
+
+print(orang1.__dict__)
+print(orang2.__dict__)
+print(orang2.nama)
+print(orang2.usia)
