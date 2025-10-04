2224b0d5974a6c715f9a1ba426ed1108e55e1472
PaulD4vd <pauldaviddjukardi@gmail.com>
2025-10-04 15:26:01 +0700

    latihan_oop final

--- latihan-oop.py
+++ latihan-oop.py
@@ -8,13 +8,31 @@ class Hero:
         self.armor = defence_power
     
     def serang(self, lawan):
-        print(self.nama + " menyerang " + lawan.nama)
-        lawan.diserang(self)
+        print(self.nama + " menyerang " + lawan.nama) # sniper menyerang goliath
+        lawan.diserang(self, self.kekuatan) #lawan di serang sniper | self = sniper
 
-    def diserang(self, lawan):
-        print(self.nama + " di serang " + lawan.nama)
+    def diserang(self, lawan, kekuatan_lawan):
+        print(self.nama + " di serang " + lawan.nama) # goliat di serang lawan
+        damage = max(0, kekuatan_lawan - self.armor)
+        print("damage terasa : " + str(damage))
+        self.kesehatan -= damage
+        print("darah tersisa : " + str(self.kesehatan))
+        print("\n")
 
-sniper = Hero('Sniper', 100, 10, 5)
-goliath = Hero('goliath', 100, 5, 10)
+sniper = Hero('Sniper', 100, 15, 5)
+goliath = Hero('goliath', 100, 8, 12)
 
-sniper.serang(goliath)
+# battle 
+giliran = 0 # sniper = 0, goliath = 1
+while sniper.kesehatan > 0 and goliath.kesehatan > 0:
+    if giliran == 0:
+        sniper.serang(goliath)
+        giliran += 1
+    else: 
+        goliath.serang(sniper)
+        giliran -= 1
+
+if sniper.kesehatan <= 0:
+    print("goliath menang")
+else: 
+    print("sniper menang")
